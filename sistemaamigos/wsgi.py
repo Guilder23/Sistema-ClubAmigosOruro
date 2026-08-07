65a137f5e87d265965dbc9d75db08e943f8486f3
GUILDER <pplg39395@gmail.com>
2026-08-07 09:25:16 -0400

    Cambios en el setings

--- sistemaamigos/wsgi.py
+++ sistemaamigos/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sistema_textil project.
+WSGI config for sistemaamigos project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema_textil.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistemaamigos.settings')
 
 application = get_wsgi_application()
